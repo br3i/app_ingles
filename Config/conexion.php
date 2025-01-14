2b453c6814188cc16a9a5ce4cf0c8b62090104b3
Bryan Alexis Reinoso Guerra <b.alexis98@hotmail.es>
2025-01-14 12:57:44 -0500

    Conexión 1 - zeabur

--- Config/conexion.php
+++ Config/conexion.php
@@ -1,12 +1,25 @@
 <?php
-$host = 'localhost';
+$host = 'free.clusters.zeabur.com';
 $user = 'root';
-$pass = '';
-$db = 'app_ingles';
+$pass = 'dJap1XjSN87m0yeT9M436qEQzR25hOoG';
+$db = 'zeabur';
+$port = '31691';
 
-$con = mysqli_connect($host, $user, $pass, $db);
+$con = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$con) {
     die('Error de conexión: ' . mysqli_connect_error());
 }
+?>
+<?php
+// $host = 'localhost';
+// $user = 'root';
+// $pass = '';
+// $db = 'app_ingles';
+
+// $con = mysqli_connect($host, $user, $pass, $db);
+
+// if (!$con) {
+//     die('Error de conexión: ' . mysqli_connect_error());
+// }
 ?>
